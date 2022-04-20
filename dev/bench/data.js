6b54194caca853fc50b2fa6fd134763ef73dfb07
github-action-benchmark <github@users.noreply.github.com>
2022-04-20 04:17:40 +0000

    add Benchmark (go) benchmark result for ed21f63fdb332d948aa8e49843c6aa9dd4773a12

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1650428260800,
+  "repoUrl": "https://github.com/posteris/commons",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "gsdenys@gmail.com",
+            "name": "Denys G. Santos",
+            "username": "gsdenys"
+          },
+          "committer": {
+            "email": "gsdenys@gmail.com",
+            "name": "Denys G. Santos",
+            "username": "gsdenys"
+          },
+          "distinct": true,
+          "id": "ed21f63fdb332d948aa8e49843c6aa9dd4773a12",
+          "message": "add bench support",
+          "timestamp": "2022-04-20T01:09:35-03:00",
+          "tree_id": "911dff7c1e88cac53dc65671ef2d1875d41ce996",
+          "url": "https://github.com/posteris/commons/commit/ed21f63fdb332d948aa8e49843c6aa9dd4773a12"
+        },
+        "date": 1650428260366,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkValidateModel/Errorless",
+            "value": 0.0000441,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkValidateModel/Error",
+            "value": 0.000202,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n2 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
